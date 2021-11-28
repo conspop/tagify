6eb5ffc6dff5938e048a763f74aa3add2de7fd16
Seb <you@example.com>
2021-11-28 11:29:45 -0500

    Implement black formatting

--- spotify/api.py
+++ spotify/api.py
@@ -6,11 +6,12 @@ response_type = "code"
 redirect_uri = "http://localhost:8000/spotify/callback"
 scopes = "user-library-read"
 
+
 def authorize_user():
     payload = {
-        "response_type":response_type,
-        "client_id":client_id,
-        "scope":scopes,
-        "redirect_uri":redirect_uri
+        "response_type": response_type,
+        "client_id": client_id,
+        "scope": scopes,
+        "redirect_uri": redirect_uri,
     }
     redirect("https://accounts.spotify.com/authorize?", **payload)
